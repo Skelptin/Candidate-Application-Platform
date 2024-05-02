c4b36d55df0ded31d3b059cb0e6fc54b35f25eb0
Mayank Painkra <mpainkra7@gmail.com>
2024-05-02 23:53:00 +0530

    setup redux and created cards layout

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FaHourglassHalf } from "react-icons/fa6";
 
+import img from '../assets/react.svg'
 import './Cards.css';
 
 const Cards = ({ job }) => {
@@ -9,13 +10,18 @@ const Cards = ({ job }) => {
     return (
         <div className="card">
             <div className='postInfo'>
-                <FaHourglassHalf /> Posted 10 days ago
+                <FaHourglassHalf /> &nbsp; Posted 10 days ago
             </div>
-            <div className='jobInfo'>
+            <div className='job'>
+                <img src={img} alt='logo' />
+                <div className='jobInfo'>
+                    <h3 className="card-title">Lorem Ipsum</h3>
+                    <p className='card-role'>{job.jobRole}</p>
+                    <p className='card-location'>{job.location}</p>
+                </div>
+            </div>
 
-            </div>
-            <h3 className="card-title">Lorem Upsum</h3>
-            <p className="card-company">co</p>
+            <p className="card-salary">Estimated Salary: &nbsp; ₹{job.minJdSalary == null ? 10 : job.minJdSalary} - {job.maxJdSalary} LPA</p>
             <p className="card-location">loc</p>
             <p className="card-description">descri</p>
         </div>
